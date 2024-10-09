2714a5663bccc35498f107b116639668398b35a7
rhit-mooneyrr <mooneyrr@rose-hulman.edu>
2024-10-08 23:43:45 -0400

    made a fitness function?

--- Hyena.py
+++ Hyena.py
@@ -1,8 +1,17 @@
 import numpy as np
-import matplotlib as plt
 
 class Hyena():
 
-    def __init__(self, fitnessfunction, popsize):
+    def __init__(self, target, fitnessfunction, popsize, mutation_rate, generations):
+        self.target = np.array(target)
         self.fitnessfunction = fitnessfunction
         self.popsize = popsize
+        self.mutation_rate = mutation_rate
+        self.generations = generations
+
+    def fitness(self, genome):
+        position = np.zeros(2)
+        position += genome
+        distance = np.linalg.norm(self.target - position)
+        return 1 / (1 + distance)
+    
